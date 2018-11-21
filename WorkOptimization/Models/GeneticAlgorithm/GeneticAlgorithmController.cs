1d26ae892cab0822fbc73bbd3e6bfca0439d2934
Michalb96 <bugajski.michal96@gmail.com>
2018-11-21 19:21:14 +0100

    Mutation function ready

--- WorkOptimization/Models/GeneticAlgorithm/GeneticAlgorithmController.cs
+++ WorkOptimization/Models/GeneticAlgorithm/GeneticAlgorithmController.cs
@@ -17,14 +17,13 @@ namespace WorkOptimization.Models.GeneticAlgorithm
         private int _employeesNumber;
         private int _sizeOfPopulation;
         private int _numberOfIterations;
-        private double _mutationRate;
+        private double _mutationRate; 
         private double _percentageOfChildrenFromPreviousGeneration;
         private double _percentageOfParentsChosenToSelection;
         private FactoryController _factory;
         private List<int> _iterationResults;
         private Subjects _subjects;
         public List<Specimen> Population { get; set; }
-        private List<Machines> _machineList;
         private List<Employees> _employeesList;
 
         public GeneticAlgorithmController(GeneticAlgorithmParameters algorithmParameteres, FactoryController factory)
@@ -36,9 +35,6 @@ namespace WorkOptimization.Models.GeneticAlgorithm
             _percentageOfChildrenFromPreviousGeneration = algorithmParameteres.PercentageOfChildrenFromPreviousGeneration;
             _percentageOfParentsChosenToSelection = algorithmParameteres.PercentageOfParentsChosenToSelection;
             _factory = factory;
-            _machineList = factory.MachinesList;
-            _employeesList = factory.EmployeesList;
-            _iterationResults = new List<int>();
             _subjects = new Subjects(factory);
             _subjects.MakeSubject();
 
@@ -49,7 +45,7 @@ namespace WorkOptimization.Models.GeneticAlgorithm
         private void CreatePopulation(int sizeOfPopulation, int employeesNumber, Subjects subjects)
         {
             var profitList = new List<double>();
-            var sorted =  _employeesList.OrderBy(o => o.Abilities).ToList();
+            var sorted =  _factory.EmployeesList.OrderBy(o => o.Abilities).ToList();
             for (int i = 0; i < sizeOfPopulation; i++)
             {
                 Specimen specimen = new Specimen();
@@ -65,21 +61,52 @@ namespace WorkOptimization.Models.GeneticAlgorithm
                         }
                     }
                     int k = _randomNumber.Next(tempList.Count);
-                    //Machines chosen = _machineList[k];
-                    //while (specimen.Genome.ContainsKey(_machineList[k]))
-                    while (specimen.Genome.ContainsKey(_machineList[tempList[k]]))
+                    while (specimen.Genome.ContainsKey(_factory.MachinesList[tempList[k]]))
                     {
                         k = _randomNumber.Next(tempList.Count);
                     }
-                    specimen.Genome.Add(_machineList[tempList[k]], employee);
+                    specimen.Genome.Add(_factory.MachinesList[tempList[k]], employee);
                     profitList.Add(ObjectiveFunctionCounter.CountValueOfTheFunction(_factory,specimen));
                 }
-                Population.Add(specimen);
+                Population.Add(specimen);              
             }
+            Mutate(Population, _mutationRate);
         }
         
+        public void Crossover()
+        {
+
+        }
+
+        public void Mutate(List<Specimen> population, double mutationRate)
+        {
+            for(int i =0; i < population.Count; i++)
+            {
+                double k = _randomNumber.NextDouble();
+                if ( k  < _mutationRate)
+                {
+                    population[i].Genome = MutateSpecimen(population[i].Genome);
+                }
+            }
+        }
+
+        public Dictionary<Machines, Employees> MutateSpecimen(Dictionary<Machines, Employees> genome)
+        {
+            var genomeList = genome.ToList();
+            int k1 = _randomNumber.Next(genome.Count);
+            var machineNumber = _factory.MachinesList.IndexOf(genomeList[k1].Key);
+            var employeesList = _factory.EmployeesList.FindAll(
+                                s => s.VectorOfAbilities[machineNumber] == '1' && s.EmployeeID != genomeList[k1].Value.EmployeeID);
+            int k2 = _randomNumber.Next(employeesList.Count);
+            var machine_1 = genomeList.Find(s => s.Value.EmployeeID == employeesList[k2].EmployeeID);
+            genome[genomeList[k1].Key] = employeesList[k2];
+            genome[machine_1.Key] = genomeList[k1].Value;
+
+            return genome;
+        }
     }
 }
+ 
 /*
  Specimen specimen = new Specimen();
                 for (int j = 0; j< employeesNumber; j++)

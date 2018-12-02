c201d272febd2604b8f5774e486971f26bfc5c59
Michalb96 <bugajski.michal96@gmail.com>
2018-12-03 00:13:02 +0100

    Creating MainWindow

--- WorkOptimization/MainWindow.xaml.cs
+++ WorkOptimization/MainWindow.xaml.cs
@@ -17,8 +17,9 @@ namespace WorkOptimization
             FactoryController Factory = FactoryController.Create();
             GeneticAlgorithmParameters Parameters = GeneticAlgorithmParameters.Create(25,20,100,1,0.2,2);
             //GeneticAlgorithmController Controller = new GeneticAlgorithmController(Parameters,Factory);
-            BeesAlgorithmParameters BeesParameters = BeesAlgorithmParameters.Create(25, 500, 5000,0.001,0.001);
+            BeesAlgorithmParameters BeesParameters = BeesAlgorithmParameters.Create(25, 50, 500,0.001,0.001);
             BeesAlgorithmController BeeController = new BeesAlgorithmController(BeesParameters,Factory);
         }
+
     }
 }
